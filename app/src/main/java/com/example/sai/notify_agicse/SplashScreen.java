5cca35053097edbe53ece40cb945faab18e8dc86
Sai <saiswaroop7@gmail.com>
2017-09-13 22:31:11 +0530

    Cloud Functions implemented ; UI changed Data Added to Dev Change email contents for each event in index.js

--- app/src/main/java/com/example/sai/notify_agicse/SplashScreen.java
+++ app/src/main/java/com/example/sai/notify_agicse/SplashScreen.java
@@ -10,19 +10,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    Thread myThread=new Thread(){
-        @Override
-        public void run() {
-            try {
-                sleep(2000);
-                Intent intent=new Intent(getApplicationContext(),Login.class);
-                startActivity(intent);
-                finish();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-    };
+        };
         myThread.start();
     }
 }
